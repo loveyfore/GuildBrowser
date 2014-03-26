f49138980e24e062e1e2256a823947b0d109706c
loveyfore@163.com <loveyfore@163.com>
2014-03-26 11:17:14 +0800

    dsdssd

---
+++ GuildBrowserLogicTests/WowUtilsTests.h
@@ -0,0 +1,13 @@
+//
+//  WowUtilsTests.h
+//  GuildBrowser
+//
+//  Created by jin yonghua on 14-3-26.
+//  Copyright (c) 2014年 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTests : SenTestCase
+
+@end
