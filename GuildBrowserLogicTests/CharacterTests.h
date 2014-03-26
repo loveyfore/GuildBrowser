e39d1580a3d4dcf1b47056f70d7956fb909af29b
loveyfore@163.com <loveyfore@163.com>
2014-03-26 11:41:26 +0800

    heihei

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by jin yonghua on 14-3-26.
+//  Copyright (c) 2014年 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
